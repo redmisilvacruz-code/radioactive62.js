fa4ed0fee969bf05523fce56534536e9684c45d5
redmisilvacruz-code <redmisilvacruz@gmail.com>
2025-08-08 15:42:54 -0300

    dcite.js

---
+++ dcite.js
@@ -0,0 +1,9 @@
+elements.lorenzonio_liquid = {
+  color: "#00ff00",
+  behavior: behaviors.LIQUID,
+  state: "liquid",
+  category: "radioactive",
+  density: 1200,
+  temp: 25,
+  radioactive: true
+}
